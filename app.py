03b6230aad946ccaad28b77578f8f1fd19e66b64
darshanwb <124980776+darshanwb@users.noreply.github.com>
2024-06-25 15:39:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,4 +100,5 @@ def remove_folder():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
